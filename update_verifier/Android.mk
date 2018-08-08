LineageOS/android_bootable_recovery

update_verifier: skip verity to determine successful on lineage builds
Change-Id: I27a89df652febb312971a9afdb4dc37f8adb0b2e

update_verifier/Android.mk
@@ -24,8 +24,10 @@ LOCAL_SHARED_LIBRARIES := libbase libcutils libhardware liblog
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/..
 
+ifeq ($(CM_BUILD),)
 ifeq ($(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_SUPPORTS_VERITY),true)
     LOCAL_CFLAGS += -DPRODUCT_SUPPORTS_VERITY=1
+endif
 endif
 
 include $(BUILD_EXECUTABLE)

IAmTheOneTheyCallNeo/bootable_recovery

update_verifier/Android.mk
@@ -35,8 +35,10 @@ LOCAL_EXPORT_C_INCLUDE_DIRS := \
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/include
 
+ifeq ($(LINEAGE_BUILD),)
 ifeq ($(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_SUPPORTS_VERITY),true)
 LOCAL_CFLAGS += -DPRODUCT_SUPPORTS_VERITY=1
+endif
 endif
 
 ifeq ($(BOARD_AVB_ENABLE),true)